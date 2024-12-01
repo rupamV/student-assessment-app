e79716d2df6c4e7fabdd505a80d1509a8b50e8d8
rupamV <rupammvallecha@outlook.com>
2024-12-01 18:01:24 +0530

    modified:   src/app/app.routes.ts

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,30 +1,32 @@
 import { Routes } from '@angular/router';
-import { ExaminerDashboardComponent } from './components/examiner-dashboard/examiner-dashboard.component';
-import { StudentDashboardComponent } from './components/student-dashboard/student-dashboard.component';
 import { AuthGuard } from './auth/auth.guard';
-import { AddExamComponent } from './components/examiner-dashboard/add-exam/add-exam.component';
-import { GiveVerdictComponent } from './components/examiner-dashboard/give-verdict/give-verdict.component';
-import { EditExamComponent } from './components/examiner-dashboard/edit-exam/edit-exam.component';
-import { TakeExamComponent } from './components/student-dashboard/take-exam/take-exam.component';
-import { AuthComponent } from './auth/auth.component';
-import { HomepageComponent } from './components/homepage/homepage.component';
 
 export const routes: Routes = [
   {
     path: '',
-    component: HomepageComponent,
+    loadChildren: () =>
+      import('./components/homepage/homepage.component').then(
+        (m) => m.HomepageComponent
+      ),
   },
   {
     path: 'homepage',
-    component: HomepageComponent,
+    loadChildren: () =>
+      import('./components/homepage/homepage.component').then(
+        (m) => m.HomepageComponent
+      ),
   },
   {
     path: 'auth',
-    component: AuthComponent,
+    loadChildren: () =>
+      import('./auth/auth.component').then((m) => m.AuthComponent),
   },
   {
     path: 'dashboard/examiner',
-    component: ExaminerDashboardComponent,
+    loadChildren: () =>
+      import(
+        './components/examiner-dashboard/examiner-dashboard.component'
+      ).then((m) => m.ExaminerDashboardComponent),
     canActivate: [AuthGuard],
     data: {
       role: 'examiner',
@@ -32,19 +34,31 @@ export const routes: Routes = [
   },
   {
     path: 'dashboard/examiner/add-exam',
-    component: AddExamComponent,
+    loadChildren: () =>
+      import(
+        './components/examiner-dashboard/add-exam/add-exam.component'
+      ).then((m) => m.AddExamComponent),
   },
   {
     path: 'dashboard/examiner/edit-exam/:examId',
-    component: EditExamComponent,
+    loadChildren: () =>
+      import(
+        './components/examiner-dashboard/edit-exam/edit-exam.component'
+      ).then((m) => m.EditExamComponent),
   },
   {
     path: 'dashboard/examiner/:examId',
-    component: GiveVerdictComponent,
+    loadChildren: () =>
+      import(
+        './components/examiner-dashboard/give-verdict/give-verdict.component'
+      ).then((m) => m.GiveVerdictComponent),
   },
   {
     path: 'dashboard/student',
-    component: StudentDashboardComponent,
+    loadChildren: () =>
+      import('./components/student-dashboard/student-dashboard.component').then(
+        (m) => m.StudentDashboardComponent
+      ),
     canActivate: [AuthGuard],
     data: {
       role: 'student',
@@ -52,7 +66,10 @@ export const routes: Routes = [
   },
   {
     path: 'dashboard/student/take-exam/:examId',
-    component: TakeExamComponent,
+    loadChildren: () =>
+      import(
+        './components/student-dashboard/take-exam/take-exam.component'
+      ).then((m) => m.TakeExamComponent),
     canActivate: [AuthGuard],
     data: {
       role: 'student',
